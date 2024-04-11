55971f5b56b9ee4092b9c92ce6445be02c7da811
Mendes Hugo <mendeshugo98@gmail.com>
2024-04-11 09:32:18 +0200

    Add a devcontainer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
-	imports: [RouterOutlet],
 	selector: "app-root",
 	standalone: true,
 	styleUrl: "./app.component.scss",
 	templateUrl: "./app.component.html",
+	
+	imports: [RouterOutlet],
 })
 export class AppComponent {
 	public title = "frontend";
