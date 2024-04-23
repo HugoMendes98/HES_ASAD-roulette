830591ac3a40a2e8037916942c865ebd1bf5064c
Mendes Hugo <hugo.mendes@noreply.github.com>
2024-04-23 18:09:16 +0200

    chore: run linter (on ts & html files)

--- frontend/src/app/game/views/game/game.view.ts
+++ frontend/src/app/game/views/game/game.view.ts
@@ -1,16 +1,15 @@
 import { CommonModule } from "@angular/common";
 import { Component, input, OnInit } from "@angular/core";
-import { toObservable, toSignal } from "@angular/core/rxjs-interop";
+import { toObservable } from "@angular/core/rxjs-interop";
 import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
+import $ from "jquery";
 import { switchMap } from "rxjs";
 
 import { ApiModule, GameApiService } from "../../../../lib/api";
 import { SocketModule, SocketService } from "../../../../lib/socket";
-import $ from 'jquery';
 import { AuthModule } from "../../../auth/auth.module";
-import { AuthService } from "../../../auth/auth.service";
 
 @Component({
 	standalone: true,
@@ -43,19 +42,18 @@ export class GameView implements OnInit {
 		}),
 	);
 
-
 	protected fakesBetsFromBackEnd: any = {
 		"11": {
-			"username": "anthony",
-			"value": 1
+			username: "anthony",
+			value: 1,
 		},
 		"14": {
-			"username": "anthony",
-			"value": 10
+			username: "anthony",
+			value: 10,
 		},
 		"15": {
-			"username": "toto",
-			"value": 10
+			username: "toto",
+			value: 10,
 		},
 	};
 
@@ -67,11 +65,11 @@ export class GameView implements OnInit {
 	public constructor(
 		private readonly gameApi: GameApiService,
 		private readonly socket: SocketService,
-	) { }
+	) {}
 	ngOnInit(): void {
 		this.setupEvent();
-		let myThis = this;
-		document.addEventListener('mousemove', e => {
+		const myThis = this;
+		document.addEventListener("mousemove", e => {
 			myThis.updateCursorPosition(e.clientX, e.clientY);
 		});
 
@@ -80,27 +78,25 @@ export class GameView implements OnInit {
 
 			this.updateView();
 
-			if (this.previousState == "BIDABLE" && state["state"] == "WAITING") {
+			if (
+				this.previousState == "BIDABLE" &&
+				state["state"] == "WAITING"
+			) {
 				_window().spinWheel(state["winning_slot"]);
 				console.log("toto");
-
 			}
 			this.previousState = state["state"];
-
-
-		})
+		});
 	}
 
 	//API POST a bet for a user
 	public bid(id: any, value: number) {
-		void this.gameApi
-			.addBid(this.gameId(), {
-				username: this.username,
-				position_id: id,
-				value: value
-			});
+		void this.gameApi.addBid(this.gameId(), {
+			position_id: id,
+			username: this.username,
+			value: value,
+		});
 	}
-
 
 	/** Change the chip selected by the user */
 	public changeBid(value: number) {
@@ -108,40 +104,39 @@ export class GameView implements OnInit {
 	}
 	/**
 	 * Add a chip on the view in the correct case
-	 * @param elementClicked 
-	 * @param valueBet 
+	 * @param elementClicked
+	 * @param valueBet
 	 */
-	public addChip(elementClicked: { dataset: { num: string | undefined; sector: string; }; }) {
-		let valueBet = this.amountSelected
+	public addChip(elementClicked: {
+		dataset: { num: string | undefined; sector: string };
+	}) {
+		const valueBet = this.amountSelected;
 		let title;
-		if (elementClicked.dataset.num != undefined) {
-			title = "num-" + elementClicked.dataset.num;
-		}
-		else {
-			let dataSelected = elementClicked.dataset.sector.split(",");
-			title = "sector-" + elementClicked.dataset.sector;
+		if (elementClicked.dataset.num == undefined) {
+			const dataSelected = elementClicked.dataset.sector.split(",");
+			title = `sector-${elementClicked.dataset.sector}`;
+		} else {
+			title = `num-${elementClicked.dataset.num}`;
 		}
 		this.bid(title, valueBet);
 		this.updateView();
 	}
 
-
 	/**
 	 * Setup Event onClick on each case to allow add chip on case
 	 */
 	private setupEvent() {
-		$(".controlls-2 table").on("mousedown", (e: { target: any; }) => {
+		$(".controlls-2 table").on("mousedown", (e: { target: any }) => {
 			let elementClicked = e.target;
 			if (elementClicked.tagName == "SPAN") {
 				elementClicked = elementClicked.parentElement;
 			}
 
 			//Need to take care of sector, multiple sector and num later
-			let number = elementClicked.dataset.num;
+			const number = elementClicked.dataset.num;
 			if (this.caseAvailable(number)) {
 				this.addChip(elementClicked);
 			}
-
 		});
 	}
 
@@ -149,18 +144,22 @@ export class GameView implements OnInit {
 	 * Update all chip in the views
 	 */
 	private updateView() {
-
 		//Prepare the data as used (temp only work with number)
-		let bets = new Map()
-		Object.keys(this.fakesBetsFromBackEnd).forEach(el => {
-			bets.set("num-" + el, { value: this.fakesBetsFromBackEnd[el].value, username: this.fakesBetsFromBackEnd[el].username, htmlElement: $(`*[data-num=${el}]`)[0] })
-		})
+		const bets = new Map();
+		for (const el of Object.keys(this.fakesBetsFromBackEnd)) {
+			bets.set(`num-${el}`, {
+				htmlElement: $(`*[data-num=${el}]`)[0],
+				username: this.fakesBetsFromBackEnd[el].username,
+				value: this.fakesBetsFromBackEnd[el].value,
+			});
+		}
 
 		_window().updateBetsView(bets, this.username);
 	}
 	//Update positon of the chip selected based on the cursor position
 	private updateCursorPosition(clientX: number, clientY: number) {
-		let cursorElement: any | null = document.getElementById("cursorElement");
+		const cursorElement: any | null =
+			document.getElementById("cursorElement");
 		switch (this.amountSelected) {
 			case 0:
 				cursorElement.innerHTML = "";
@@ -184,28 +183,24 @@ export class GameView implements OnInit {
 			default:
 				break;
 		}
-		cursorElement.style.left = (clientX - 10) + 'px'; // Adjusting for element width
-		cursorElement.style.top = (clientY - 10) + 'px'; // Adjusting for element height
+		cursorElement.style.left = `${clientX - 10}px`; // Adjusting for element width
+		cursorElement.style.top = `${clientY - 10}px`; // Adjusting for element height
 	}
 
 	private caseAvailable(number: any) {
 		if (this.fakesBetsFromBackEnd[number] == undefined) {
 			return true;
-		}
-		else {
+		} else {
 			if (this.fakesBetsFromBackEnd[number].username == this.username) {
-				return true
+				return true;
 			}
 		}
-		alert("Case not available")
+		alert("Case not available");
 		return false;
-
 	}
 }
-
 
 function _window(): any {
 	// return the global native browser window object
 	return window;
 }
-
