5e83db6fa4e95381acdc4057c8d3a2569a354d8e
Alban <fav.alban2@gmail.com>
2024-05-18 19:03:07 +0200

    improved model

--- backend/src/models/game.py
+++ backend/src/models/game.py
@@ -6,61 +6,59 @@ from .round_info import RoundStates
 # technically represent a "table"
 # maybe ad a "closed" state
 class Game(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
 
-	def __repr__(self):
-		return "<Game %r>" % self.id
+    def __repr__(self):
+        return "<Game %r>" % self.id
 
-	@classmethod
-	def get(cls, game_id):
-		return db.session.query(cls).filter_by(id=game_id).one_or_none()
+    @classmethod
+    def get(cls, game_id):
+        return db.session.query(cls).filter_by(id=game_id).one_or_none()
 
-	@classmethod
-	def new(cls, id_=None):
-		new_game = cls(id=id_)
-		db.session.add(new_game)
-		db.session.commit()
-		return new_game
+    @classmethod
+    def new(cls, id_=None):
+        new_game = cls(id=id_)
+        db.session.add(new_game)
+        db.session.commit()
+        return new_game
 
-	def get_last_round(self) -> Round:
-		return (
-			db.session.query(Round)
-			.filter_by(game=self)
-			.order_by(Round.round_number.desc())
-			.first()
-		)
+    def get_last_round(self) -> Round:
+        return (
+            db.session.query(Round)
+            .filter_by(game=self)
+            .order_by(Round.round_number.desc())
+            .first()
+        )
 
-	def go_to_idle(self, next_state_timestamp=None):
-		previous_round = self.get_last_round()
-		if not previous_round:
-			Round.new(
-				round_number=1,
-				game=self,
-				next_state_timestamp=next_state_timestamp,
-			)
-		else:
-			Round.new(
-				game=self,
-				round_number=previous_round.round_number + 1,
-				next_state_timestamp=next_state_timestamp,
-			)
-		# default to idle so no need
-		# Round.update_state(round_id=previous_round.id, new_state=RoundStates.IDLE.value)
+    def go_to_idle(self, next_state_timestamp=None):
+        previous_round = self.get_last_round()
+        if not previous_round:
+            Round.new(
+                round_number=1,
+                game=self,
+                next_state_timestamp=next_state_timestamp,
+            )
+        else:
+            Round.new(
+                game=self,
+                round_number=previous_round.round_number + 1,
+                next_state_timestamp=next_state_timestamp,
+            )
+        # default to idle so no need
+        # Round.update_state(round_id=previous_round.id, new_state=RoundStates.IDLE.value)
 
-	def go_to_bidable(self, next_state_timestamp=None):
-		current_round = self.get_last_round()
-		# Round.new(game=self, round_number=current_round.round_number + 1)
-		current_round.update_state(RoundStates.BIDABLE, next_state_timestamp)
+    def go_to_bidable(self, next_state_timestamp=None):
+        current_round = self.get_last_round()
+        # Round.new(game=self, round_number=current_round.round_number + 1)
+        current_round.update_state(RoundStates.BIDABLE, next_state_timestamp)
 
-	def go_to_waiting(self, next_state_timestamp=None):
-		current_round = self.get_last_round()
-		current_round.update_state(RoundStates.WAITING, next_state_timestamp)
+    def go_to_waiting(self, next_state_timestamp=None):
+        current_round = self.get_last_round()
+        current_round.update_state(RoundStates.WAITING, next_state_timestamp)
 
-	def go_to_result(self, winning_slot, next_state_timestamp=None):
-		current_round = self.get_last_round()
-		Round.update_winning_slot(
-			round_id=current_round.id, new_winning_slot=winning_slot
-		)
-		current_round.update_state(RoundStates.RESULT, next_state_timestamp)
-		current_round.update_bids_after_result()
-		current_round.pay_out()
+    def go_to_result(self, winning_slot, next_state_timestamp=None):
+        current_round = self.get_last_round()
+        current_round.update_winning_slot(new_winning_slot=winning_slot)
+        current_round.update_state(RoundStates.RESULT, next_state_timestamp)
+        current_round.update_bids_after_result()
+        current_round.pay_out()
--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -4,288 +4,284 @@ import pytest
 
 from src.app import create_app
 from src.models import (
-	Bid,
-	Game,
-	Round,
-	User,
-	db,
+    Bid,
+    Game,
+    Round,
+    User,
+    db,
 )
 from src.models.round_info import (
-	InOutBets,
-	RoundStates,
-	Slots,
+    InOutBets,
+    RoundStates,
+    Slots,
 )
 
 
 @pytest.fixture
 def client():
-	app = create_app()
-	app.testing = True
-	with app.test_client() as testclient:
-		with app.app_context():
-			db.create_all()
-			yield testclient
+    app = create_app()
+    app.testing = True
+    with app.test_client() as testclient:
+        with app.app_context():
+            db.create_all()
+            yield testclient
 
 
 def test_users(client):
-	m = User.new(username="molasse")
-	assert m.username == "molasse"
+    m = User.new(username="molasse", password="epave")
+    assert m.username == "molasse"
 
 
 def test_bid_wager_update(client):
-	g = Game.new()
-	u = User.new(password_hash="asdkdlj", username="Jean Test")
-	r = Round.new(round_number=1, game=g)
-	b = Bid.new(inOutbet=InOutBets.FIFTEEN, wager=10, user=u, round=r)
-	Bid.update_wager(b.id, 20)
-	assert b.wager == 20
+    g = Game.new()
+    u = User.new(password="asdkdlj", username="Jean Test")
+    r = Round.new(round_number=1, game=g)
+    b = Bid.new(inOutbet=InOutBets.FIFTEEN, wager=10, user=u, round=r)
+    b.update_wager(20)
+    assert b.wager == 20
 
 
 def test_perfect_bid_removal(client):
-	g = Game.new()
-	u = User.new(password_hash="asdkdlj", username="Jean Zero")
-	g.go_to_idle()
-	g.go_to_bidable()
+    g = Game.new()
+    u = User.new(password="asdkdlj", username="Jean Zero")
+    g.go_to_idle()
+    g.go_to_bidable()
 
-	assert u.balance == 200
+    assert u.balance == 200
 
-	b_1 = u.bet(game=g, player_bet=InOutBets.TEN, wager=10)
+    b_1 = u.bet(game=g, player_bet=InOutBets.TEN, wager=10)
 
-	assert b_1.wager == 10
-	assert u.balance == 190
+    assert b_1.wager == 10
+    assert u.balance == 190
 
-	b_2 = u.bet(game=g, player_bet=InOutBets.TEN, wager=-10)
+    b_2 = u.bet(game=g, player_bet=InOutBets.TEN, wager=-10)
 
-	assert not b_2  # bid should have been deleted
-	assert u.balance == 200
+    assert not b_2  # bid should have been deleted
+    assert u.balance == 200
 
 
 def test_negative_bet_cheating(client):
-	g = Game.new()
-	u = User.new(password_hash="asdkdlj", username="Jean Cheat")
-	g.go_to_idle()
-	g.go_to_bidable()
+    g = Game.new()
+    u = User.new(password="asdkdlj", username="Jean Cheat")
+    g.go_to_idle()
+    g.go_to_bidable()
 
-	assert u.balance == 200
+    assert u.balance == 200
 
-	b_1 = u.bet(game=g, player_bet=InOutBets.TEN, wager=10)
+    b_1 = u.bet(game=g, player_bet=InOutBets.TEN, wager=10)
 
-	assert b_1.wager == 10
-	assert u.balance == 190
+    assert b_1.wager == 10
+    assert u.balance == 190
 
-	b_2 = u.bet(game=g, player_bet=InOutBets.TEN, wager=-15)
+    b_2 = u.bet(game=g, player_bet=InOutBets.TEN, wager=-15)
 
-	assert not b_2  # bid should have been deleted
-	assert u.balance == 200
+    assert not b_2  # bid should have been deleted
+    assert u.balance == 200
 
 
 def test_winning(client):
-	g = Game.new()
-	u = User.new(password_hash="asdkdlj", username="Jean Cheat")
-	g.go_to_idle()
-	g.go_to_bidable()
+    g = Game.new()
+    u = User.new(password="asdkdlj", username="Jean Cheat")
+    g.go_to_idle()
+    g.go_to_bidable()
 
-	assert u.balance == 200
+    assert u.balance == 200
 
-	b_1 = u.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10)
-	b_2 = u.bet(game=g, player_bet=InOutBets.TEN, wager=10)
+    b_1 = u.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10)
+    b_2 = u.bet(game=g, player_bet=InOutBets.TEN, wager=10)
 
-	assert b_1.wager == 10
-	assert u.balance == 180
+    assert b_1.wager == 10
+    assert u.balance == 180
 
-	g.go_to_waiting()
-	g.go_to_result(Slots.ELEVEN)
+    g.go_to_waiting()
+    g.go_to_result(Slots.ELEVEN)
 
-	assert u.balance == 180 + 10 * 36
+    assert u.balance == 180 + 10 * 36
 
 
 def test_slot_reservation(client):
-	g = Game.new()
-	u_1 = User.new(password_hash="asdkdlj", username="Jean Test")
-	u_2 = User.new(password_hash="asdkdlj", username="Test Andy")
-	g.go_to_idle()
-	g.go_to_bidable()
+    g = Game.new()
+    u_1 = User.new(password="asdkdlj", username="Jean Test")
+    u_2 = User.new(password="asdkdlj", username="Test Andy")
+    g.go_to_idle()
+    g.go_to_bidable()
 
-	b_1 = u_1.bet(game=g, player_bet=InOutBets.TEN, wager=10)
-	b_2 = u_2.bet(game=g, player_bet=InOutBets.TWENTY, wager=20)
+    b_1 = u_1.bet(game=g, player_bet=InOutBets.TEN, wager=10)
+    b_2 = u_2.bet(game=g, player_bet=InOutBets.TWENTY, wager=20)
 
-	assert b_1.wager == 10
-	assert b_2.wager == 20
+    assert b_1.wager == 10
+    assert b_2.wager == 20
 
-	with pytest.raises(Exception):
-		u_1.bet(game=g, player_bet=InOutBets.TWENTY, wager=10)
+    with pytest.raises(Exception):
+        u_1.bet(game=g, player_bet=InOutBets.TWENTY, wager=10)
 
-	u_1.bet(game=g, player_bet=InOutBets.TEN, wager=10)
+    u_1.bet(game=g, player_bet=InOutBets.TEN, wager=10)
 
-	assert b_1.wager == 20
+    assert b_1.wager == 20
 
 
 def test_state_cycle(client):
-	g = Game.new()
+    g = Game.new()
 
-	for i in range(1, 100):
-		g.go_to_idle()
-		r = g.get_last_round()
-		assert r.round_number == i
-		assert r.state == RoundStates.IDLE.value
-		g.go_to_bidable()
-		r = g.get_last_round()
-		assert r.round_number == i
-		assert r.state == RoundStates.BIDABLE.value
-		g.go_to_waiting()
-		r = g.get_last_round()
-		assert r.round_number == i
-		assert r.state == RoundStates.WAITING.value
-		g.go_to_result(Slots.ZERO)
-		r = g.get_last_round()
-		assert r.round_number == i
-		assert r.state == RoundStates.RESULT.value
+    for i in range(1, 100):
+        g.go_to_idle()
+        r = g.get_last_round()
+        assert r.round_number == i
+        assert r.state == RoundStates.IDLE.value
+        g.go_to_bidable()
+        r = g.get_last_round()
+        assert r.round_number == i
+        assert r.state == RoundStates.BIDABLE.value
+        g.go_to_waiting()
+        r = g.get_last_round()
+        assert r.round_number == i
+        assert r.state == RoundStates.WAITING.value
+        g.go_to_result(Slots.ZERO)
+        r = g.get_last_round()
+        assert r.round_number == i
+        assert r.state == RoundStates.RESULT.value
 
 
 def test_payout_full(client):
-	g = Game.new()
-	u_1 = User.new(password_hash="asdkdlj", username="Jean Test")
-	u_2 = User.new(password_hash="asdkdlj", username="Test Andy")
-	# ---------------------------------------
-	g.go_to_idle()
+    g = Game.new()
+    u_1 = User.new(password="asdkdlj", username="Jean Test")
+    u_2 = User.new(password="asdkdlj", username="Test Andy")
+    # ---------------------------------------
+    g.go_to_idle()
 
-	assert not u_1.bids
+    assert not u_1.bids
 
-	r = g.get_last_round()
-	assert r.round_number == 1
-	assert r.state == RoundStates.IDLE.value
-	ts = r.timestamp
-	assert ts  # is not none ?
-	assert r.game_id == g.id
-	assert not r.winning_slot
-	assert not r.get_winning_bids()
+    r = g.get_last_round()
+    assert r.round_number == 1
+    assert r.state == RoundStates.IDLE.value
+    ts = r.timestamp
+    assert ts  # is not none ?
+    assert r.game_id == g.id
+    assert not r.winning_slot
+    assert not r.get_winning_bids()
 
-	with pytest.raises(Exception):
-		u_1.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10)
+    with pytest.raises(Exception):
+        u_1.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10)
 
-	# --------------------------------------
-	g.go_to_bidable()
+    # --------------------------------------
+    g.go_to_bidable()
 
-	assert u_1.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10)
-	assert u_2.bet(game=g, player_bet=InOutBets.TWELVE, wager=30)
+    assert u_1.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10)
+    assert u_2.bet(game=g, player_bet=InOutBets.TWELVE, wager=30)
 
-	assert u_1.balance == 190
-	assert u_2.balance == 170
+    assert u_1.balance == 190
+    assert u_2.balance == 170
 
-	assert len(u_2.bids) == 1
+    assert len(u_2.bids) == 1
 
-	b_1: Bid = Bid.get_bids_from_round_with_bet(
-		round=r, player_bet=InOutBets.ELEVEN
-	)
-	b_2: Bid = Bid.get_bids_from_round_with_bet(
-		round=r, player_bet=InOutBets.TWELVE
-	)
+    b_1: Bid = Bid.get_bids_from_round_with_bet(round=r, player_bet=InOutBets.ELEVEN)
+    b_2: Bid = Bid.get_bids_from_round_with_bet(round=r, player_bet=InOutBets.TWELVE)
 
-	assert b_1.user_id == u_1.id
-	assert b_2.user_id == u_2.id
+    assert b_1.user_id == u_1.id
+    assert b_2.user_id == u_2.id
 
-	assert not b_2.is_won
-	assert b_2.wager == 30
+    assert not b_2.is_won
+    assert b_2.wager == 30
 
-	assert u_2.bet(game=g, player_bet=InOutBets.TWELVE, wager=-20)
-	assert u_2.balance == 190
-	assert b_2.wager == 10
+    assert u_2.bet(game=g, player_bet=InOutBets.TWELVE, wager=-20)
+    assert u_2.balance == 190
+    assert b_2.wager == 10
 
-	with pytest.raises(Exception):
-		u_1.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10000)
+    with pytest.raises(Exception):
+        u_1.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10000)
 
-	r = g.get_last_round()
-	assert r.round_number == 1
-	assert r.state == RoundStates.BIDABLE.value
-	assert ts == r.timestamp
-	assert r.game_id == g.id
-	assert not r.winning_slot
-	assert not r.get_winning_bids()
+    r = g.get_last_round()
+    assert r.round_number == 1
+    assert r.state == RoundStates.BIDABLE.value
+    assert ts == r.timestamp
+    assert r.game_id == g.id
+    assert not r.winning_slot
+    assert not r.get_winning_bids()
 
-	# ------------------------------
-	g.go_to_waiting()
+    # ------------------------------
+    g.go_to_waiting()
 
-	r = g.get_last_round()
-	assert r.round_number == 1
-	assert r.state == RoundStates.WAITING.value
-	assert ts == r.timestamp
-	assert r.game_id == g.id
-	assert not r.winning_slot
-	assert not r.get_winning_bids()
+    r = g.get_last_round()
+    assert r.round_number == 1
+    assert r.state == RoundStates.WAITING.value
+    assert ts == r.timestamp
+    assert r.game_id == g.id
+    assert not r.winning_slot
+    assert not r.get_winning_bids()
 
-	with pytest.raises(Exception):
-		u_1.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10)
+    with pytest.raises(Exception):
+        u_1.bet(game=g, player_bet=InOutBets.ELEVEN, wager=10)
 
-	# -----------------------------
-	g.go_to_result(Slots.ELEVEN)
+    # -----------------------------
+    g.go_to_result(Slots.ELEVEN)
 
-	r = g.get_last_round()
-	assert r.round_number == 1
-	assert r.state == RoundStates.RESULT.value
-	assert ts == r.timestamp
-	assert r.game_id == g.id
-	assert r.winning_slot == Slots.ELEVEN.value
-	w_b = r.get_winning_bids()
-	assert len(w_b) == 1
-	assert w_b[0].is_won
+    r = g.get_last_round()
+    assert r.round_number == 1
+    assert r.state == RoundStates.RESULT.value
+    assert ts == r.timestamp
+    assert r.game_id == g.id
+    assert r.winning_slot == Slots.ELEVEN.value
+    w_b = r.get_winning_bids()
+    assert len(w_b) == 1
+    assert w_b[0].is_won
 
 
 def test_post_bid(client):
-	User.new(username="oly")
-	g = Game.get(1)
-	g.go_to_idle()
-	g.go_to_bidable()
+    User.new(username="oly", password="secure_password_yes_yes")
+    g = Game.get(1)
+    g.go_to_idle()
+    g.go_to_bidable()
 
-	r0 = client.post(
-		"/api/games/1/bet",
-		data=json.dumps(dict(position_id=0, value=5, username="oly")),
-		content_type="application/json",
-	)
-	assert r0.status_code == 201
+    r0 = client.post(
+        "/api/games/1/bet",
+        data=json.dumps(dict(position_id=0, value=5, username="oly")),
+        content_type="application/json",
+    )
+    assert r0.status_code == 201
 
-	r = client.post(
-		"/api/games/1/bet",
-		data=json.dumps(dict(position_id=1, value=5, username="oly")),
-		content_type="application/json",
-	)
-	assert r.status_code == 201
+    r = client.post(
+        "/api/games/1/bet",
+        data=json.dumps(dict(position_id=1, value=5, username="oly")),
+        content_type="application/json",
+    )
+    assert r.status_code == 201
 
-	r2 = client.post(
-		"/api/games/1/bet",
-		data=json.dumps(dict(position_id=1, value=5, username="oly")),
-		content_type="application/json",
-	)
-	assert r2.json["balance"] == 185
-	assert r2.status_code == 201
+    r2 = client.post(
+        "/api/games/1/bet",
+        data=json.dumps(dict(position_id=1, value=5, username="oly")),
+        content_type="application/json",
+    )
+    assert r2.json["balance"] == 185
+    assert r2.status_code == 201
 
-	r3 = client.post(
-		"/api/games/1/bet",
-		data=json.dumps(dict(position_id=1, value=-5, username="oly")),
-		content_type="application/json",
-	)
-	assert r3.json["balance"] == 190
-	assert r3.status_code == 201
+    r3 = client.post(
+        "/api/games/1/bet",
+        data=json.dumps(dict(position_id=1, value=-5, username="oly")),
+        content_type="application/json",
+    )
+    assert r3.json["balance"] == 190
+    assert r3.status_code == 201
 
-	r4 = client.post(
-		"/api/games/1/bet",
-		data=json.dumps(dict(position_id=1, value=-10, username="oly")),
-		content_type="application/json",
-	)
-	assert r4.json["balance"] == 195
-	assert r4.status_code == 201
+    r4 = client.post(
+        "/api/games/1/bet",
+        data=json.dumps(dict(position_id=1, value=-10, username="oly")),
+        content_type="application/json",
+    )
+    assert r4.json["balance"] == 195
+    assert r4.status_code == 201
 
 
 def test_login(client):
-	User.new(username="oly")
-	g = Game.get(1)
-	g.go_to_idle()
-	g.go_to_bidable()
-	r = client.post(
-		"/api/user/login",
-		data=json.dumps(dict(username="oly")),
-		content_type="application/json",
-	)
-	assert r.status_code == 200
+    User.new(username="oly", password="secure_password_yes_yes")
+    g = Game.get(1)
+    g.go_to_idle()
+    g.go_to_bidable()
+    r = client.post(
+        "/api/user/login",
+        data=json.dumps(dict(username="oly")),
+        content_type="application/json",
+    )
+    assert r.status_code == 200
 
-	assert r.json["username"] == "oly"
-	assert isinstance(r.json["balance"], int)
+    assert r.json["username"] == "oly"
+    assert isinstance(r.json["balance"], int)
