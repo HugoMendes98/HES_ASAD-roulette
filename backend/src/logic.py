11278ba0787062c838450249c3c7448d8fbe7bee
Alban <fav.alban2@gmail.com>
2024-05-28 14:34:20 +0200

    moved logic chunk to game to respect black board arch

--- backend/src/logic.py
+++ backend/src/logic.py
@@ -1,89 +1,39 @@
-from datetime import datetime, timedelta
-from random import randint
+from datetime import datetime
 
 from flask_socketio import SocketIO
 
-from . import config
+# from . import config
 from .models import Game
-from .models.round_info import RoundStates, Slots
 from .sockets import get_game_socket_path, register_sockets
 
 
-def event_loop(app):
-	with app.app_context():
-		socketio: SocketIO = app.socketio_instance
+def event_loop(app, game_id=1):
+    with app.app_context():
+        socketio: SocketIO = app.socketio_instance
 
-		# Wait potential socket connections
-		socketio.sleep(2)
+        # Wait potential socket connections
+        socketio.sleep(2)
 
-		current_game = Game.get(1)
+        current_game = Game.get(game_id)
 
-		#try to check if the last round is done correctly or not
-		startNewRound =True
-		current_round = current_game.get_last_round()
-		if(current_round != None): #if there are a round existing in the db
-			next_state = current_round.next_state_timestamp
-			sleep_time = (next_state - datetime.utcnow()).total_seconds()
+        current_game.init_game()
 
-			if(sleep_time > 0): #still on time 
-				startNewRound = False
-			else:
-				print("late restart a new round")
-				startNewRound = True
-				if(current_round.state != RoundStates.RESULT):
-					#revert bet
-					print("cancel latest bet")
-					current_round.cancel_round()	
+        register_sockets(socketio)
 
-		if(startNewRound):
-			current_game.go_to_idle(datetime.utcnow() + timedelta(seconds=config.IDLE_time_s))
-			
-		register_sockets(socketio)
+        print("Game is about to start")
 
-		print("Game is about to start")
+        while True:
+            current_round = current_game.get_last_round()
+            next_state = current_round.next_state_timestamp
+            sleep_time = (next_state - datetime.utcnow()).total_seconds()
 
-		while True:
-			current_round = current_game.get_last_round()
-			next_state = current_round.next_state_timestamp
-			sleep_time = (next_state - datetime.utcnow()).total_seconds()
+            state = current_round.to_json()
+            print("Emit game state:", state)
 
-			state = current_round.to_json()
-			print("Emit game state:", state)
+            app.socketio_instance.emit(get_game_socket_path(current_game), state)
 
-			app.socketio_instance.emit(
-				get_game_socket_path(current_game), state
-			)
+            print("Sleep (s):", sleep_time)
+            socketio.sleep(sleep_time)
 
-			print("Sleep (s):", sleep_time)
-			socketio.sleep(sleep_time)
+            current_game.state_machine()
 
-			# Get updated last round (with bets)
-			current_round = current_game.get_last_round()
-
-			# "State machine"
-			if RoundStates(current_round.state) == RoundStates.IDLE:
-				current_game.go_to_bidable(
-					datetime.utcnow() + timedelta(seconds=config.BIDABLE_time_s)
-				)
-				print("La manche est en préparation")
-			elif RoundStates(current_round.state) == RoundStates.BIDABLE:
-				current_game.go_to_waiting(
-					datetime.utcnow() + timedelta(seconds=config.WAITING_time_s)
-				)
-				print(
-					"Les paris sont fermés, les résultats sera annoncé dans quelque temps"
-				)
-			elif RoundStates(current_round.state) == RoundStates.WAITING:
-				current_game.go_to_result(
-					Slots(randint(0, 36)),
-					datetime.utcnow()
-					+ timedelta(seconds=config.RESULTS_time_s),
-				)
-				print("Les résultats sont tombés")
-			elif RoundStates(current_round.state) == RoundStates.RESULT:
-				current_game.go_to_idle(
-					datetime.utcnow() + timedelta(seconds=config.IDLE_time_s)
-				)
-				print(
-					"La manche est terminée, un nouveau round va bientot commencer..."
-				)
--- backend/src/models/game.py
+++ backend/src/models/game.py
@@ -1,15 +1,16 @@
+from datetime import datetime, timedelta
+from random import randint
+
 from .db import db
 from .round import Round
-from .round_info import RoundStates
+from .round_info import RoundStates, Slots
+from .. import config
 
 
 # technically represent a "table"
 # maybe ad a "closed" state
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-    def __repr__(self):
-        return "<Game %r>" % self.id
 
     @classmethod
     def get(cls, game_id):
@@ -63,3 +64,59 @@ class Game(db.Model):
         current_round.update_state(RoundStates.RESULT, next_state_timestamp)
         current_round.update_bids_after_result()
         current_round.pay_out()
+
+    def state_machine(self):
+        current_round = self.get_last_round()
+        # "State machine"
+        if RoundStates(current_round.state) == RoundStates.IDLE:
+            self.go_to_bidable(
+                datetime.utcnow() + timedelta(seconds=config.BIDABLE_time_s)
+            )
+            print("La manche est en préparation")
+        elif RoundStates(current_round.state) == RoundStates.BIDABLE:
+            self.go_to_waiting(
+                datetime.utcnow() + timedelta(seconds=config.WAITING_time_s)
+            )
+            print(
+                "Les paris sont fermés, les résultats sera annoncé dans quelque temps"
+            )
+        elif RoundStates(current_round.state) == RoundStates.WAITING:
+            self.go_to_result(
+                Slots(randint(0, 36)),
+                datetime.utcnow()
+                + timedelta(seconds=config.RESULTS_time_s),
+            )
+            print("Les résultats sont tombés")
+        elif RoundStates(current_round.state) == RoundStates.RESULT:
+            self.go_to_idle(datetime.utcnow() + timedelta(seconds=config.IDLE_time_s))
+            print(
+                "La manche est terminée, un nouveau round va bientot commencer..."
+            )
+    
+    # This method prepare the game, either for a fresh start or after a crash
+    # If a late round is detected
+    def init_game(self):
+        #try to check if the last round is done correctly or not
+        startNewIdleRound =True
+        current_round = self.get_last_round()
+        if(current_round != None): #if there are a round existing in the db
+            next_state = current_round.next_state_timestamp
+            sleep_time = (next_state - datetime.utcnow()).total_seconds()
+
+            if(sleep_time > 0): #still on time 
+                startNewIdleRound = False
+            else:
+                print("late restart a new round")
+                startNewIdleRound = True
+                if(current_round.state != RoundStates.RESULT):
+                    #revert bet
+                    print("cancel latest bet")
+                    current_round.cancel_round()
+        
+        if startNewIdleRound:
+            self.go_to_idle(datetime.utcnow() + timedelta(seconds=config.IDLE_time_s))
+
+
+
+    def __repr__(self):
+        return "<Game %r>" % self.id
